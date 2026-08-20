a6488ea23fca3380c83b8ebc7c13265d749e89c2
walternksui <walternkusi@gmail.com>
2026-08-20 23:25:55 +0200

    replace all files with new version

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -28,11 +28,11 @@ async function connect(): Promise<Db> {
 
   const adminCount = await db.collection('admins').countDocuments();
   if (adminCount === 0) {
-    const hashedPassword = bcrypt.hashSync('admin123', 10);
+    const hashedPassword = bcrypt.hashSync('nobleadmin!', 10);
     await db.collection('admins').insertOne({
       id: randomUUID(),
       name: 'Admin',
-      email: 'admin@girlsfashion.com',
+      email: 'noblestore252@gmail.com',
       password: hashedPassword,
       role: 'superadmin',
       createdAt: new Date(),
